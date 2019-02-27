66f77a42b1e5cef7d07cc346f6fe53a3e463b515
Xavier Le Tohic <xletohic@lafourchette.com>
2019-02-27 17:46:53 +0100

    Fixed wrong indent base

--- extension.js
+++ extension.js
@@ -1,10 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const indentString = require('indent-string');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -57,7 +52,7 @@ function activate(context) {
 				.slice(0, index)
 				.split(/\n/g)
 				.reverse();
-			const indentSize = stepDefinition.search(/\S|$/) + 2;
+			const indentSize = tripleQuotes.search(/\S|$/);
 			success += 1;
 			return `"""\n${indentString(`${formattedJSON}\n"""`, indentSize)}`;
 		});
